1b5dd54f177c0c7e765ce63e4a6861c7aed6c845
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-06 21:11:01 +0300

    Update aboutus.spec.ts

--- test/specs/aboutus.spec.ts
+++ test/specs/aboutus.spec.ts
@@ -4,32 +4,12 @@ import MainPage from  '../pageobjects/Main.page';
 beforeEach(() => {
    MainPage.goto();
 })
- /*
+ 
 describe('Footer AboutUs page', () => {
-    it('Should click Amsterdam Google Maps link ', async () => {
+     it('Should check Channels & calls per second', async () => {
         await MainPage.clickAcceptCookiesBtn();
         await AboutUsPage.clickFooterAboutUsBtn();
-        await AboutUsPage.locateAmsterdam();
-    });
-
-    it('Should click Denver Google Maps link ', async () => {
-        await AboutUsPage.clickFooterAboutUsBtn();
-        await AboutUsPage.locateDenver();
-    });
-
-    it('Should click Dublin Google Maps link ', async () => {
-        await AboutUsPage.clickFooterAboutUsBtn();
-        await AboutUsPage.locateDublin();
-    });
-
-    it('Should click Chicago Google Maps link ', async () => {
-        await AboutUsPage.clickFooterAboutUsBtn();
-        await AboutUsPage.locateChicago();
-    });
-
-    it('Should click Warsaw Google Maps link ', async () => {
-        await AboutUsPage.clickFooterAboutUsBtn();
-        await AboutUsPage.locateWarsaw();
+        await AboutUsPage.checkChannelsCallsPerSecond();
     });
 });
-*/
+
