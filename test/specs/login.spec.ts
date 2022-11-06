67b8f8ac1cd94fb147e8183f6e873d13c28d4e10
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-06 21:32:11 +0300

    Update login.spec.ts

--- test/specs/login.spec.ts
+++ test/specs/login.spec.ts
@@ -13,14 +13,14 @@ describe('LogIn page test', () => {
         await LoginPage.clickFooterLogInBtn();
         await LoginPage.fillLogInWithValidEmail(email);
         await LoginPage.fillLogInWithValidPassword("ZpPQ_q!!u#Lz6PZ");
-        await expect(LoginPage.logInError).toBeDisplayed();
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
     });
 
     it('Should login with invalid password which consists of 4 symbols', async () => {
         await LoginPage.clickFooterLogInBtn();
         await LoginPage.fillLogInWithValidEmail(email);
         await LoginPage.fillLogInWithInvalidPassword("!!!!");
-        await expect(LoginPage.logInError).toBeDisplayed();
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
     });
 
     it('Should login with empty credentials', async () => {
