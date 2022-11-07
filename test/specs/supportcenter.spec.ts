db8b2635a42cc1d4cce2a9d777b0524868e6901e
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-07 13:04:20 +0300

    Update supportcenter.spec.ts

--- test/specs/supportcenter.spec.ts
+++ test/specs/supportcenter.spec.ts
@@ -12,4 +12,46 @@ describe('Footer Support Center page', () => {
         await SupportCenterPage.clickAccessibleCanadaActInReferenceMaterial();
         await expect(browser).toHaveUrlContaining("/6228388-accessible-canada-act-aca-feedback-process-description");
     });
+    
+     it('Should click Configuration Guides headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickConfigurationGuides();
+        await expect(browser).toHaveUrlContaining("/133118-configuration-guides");
+    });
+
+    it('Should click Messaging Articles and Guides headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickMessagingArticlesAndGuides();
+        await expect(browser).toHaveUrlContaining("/133103-messaging-articles-and-guides");
+    });
+
+    it('Should click Voice and Fax headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickVoiceAndFax();
+        await expect(browser).toHaveUrlContaining("/133140-voice-and-fax");
+    });
+
+    it('Should click Porting Articles and Guides headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickPortingArticlesAndGuides();
+        await expect(browser).toHaveUrlContaining("/133126-porting-articles-and-guides");
+    });
+
+    it('Should click Billing & Pricing headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickBillingAndPrising();
+        await expect(browser).toHaveUrlContaining("/133135-billing-pricing");
+    });
+
+    it('Should click Programmable Wireless headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickProgrammableWireless();
+        await expect(browser).toHaveUrlContaining("/1895859-programmable-wireless");
+    });
+
+    it('Should click Tours and Videos headling', async () => {
+        await SupportCenterPage.clickFooterSupportCenterBtn();
+        await SupportCenterPage.clickToursAndVideos();
+        await expect(browser).toHaveUrlContaining("/2029093-tours-and-videos");
+    });
 });
