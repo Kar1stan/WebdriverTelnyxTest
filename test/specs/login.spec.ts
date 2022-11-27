c3205e3e34630c3667a4efaef9b3e32db20ceb4c
Kar1stan <90850632+Kar1stan@users.noreply.github.com>
2022-11-27 09:58:25 +0300

    Without env

---
+++ test/specs/login.spec.ts
@@ -0,0 +1,46 @@
+import LoginPage from  '../pageobjects/LogIn.page';
+import MainPage from  '../pageobjects/Main.page';
+import 'dotenv/config'
+
+beforeEach(() => {
+    MainPage.visitUrl();
+})
+
+describe('LogIn page test', () => {
+    it('Should login with valid credentials', async () => {
+        await MainPage.clickAcceptCookiesBtn();
+        await LoginPage.clickFooterLogInBtn();
+        await LoginPage.fillEmail(process.env.EMAIL!);
+        await LoginPage.fillPassword(process.env.PASSWORD!);
+        await LoginPage.clickSubmitBtn();
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
+    });
+
+    it('Should login with invalid password which consists of 4 symbols', async () => {
+        await LoginPage.clickFooterLogInBtn();
+        await LoginPage.fillEmail(process.env.EMAIL!);
+        await LoginPage.fillPassword(process.env.WRONG_PASSWORD_WITH_FOUR_SYMBOLS!);
+        await LoginPage.clickSubmitBtn();
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
+    });
+
+    it('Should login with empty credentials', async () => {
+        await LoginPage.clickFooterLogInBtn();
+        await LoginPage.logInWithEmptyCredentials();
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
+    });
+
+    it('Should LogIn with false company email without @ symbol in Single Sign-On', async () => { 
+        await LoginPage.clickFooterLogInBtn();
+        await LoginPage.fillCompanyEmail(process.env.COMPANY_EMAIL_WITHOUT_AT_SYMBOL!);
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
+    });
+
+    it('Should LogIn with false company email in Single Sign-On', async () => {
+        await LoginPage.clickFooterLogInBtn();
+        await LoginPage.fillBusinessName(process.env.BUSINESS_NAME_WITH_FOUR_NUMBERS!);
+        await expect(browser).toHaveUrlContaining("/login/sign-in");
+    });
+});
+
+
